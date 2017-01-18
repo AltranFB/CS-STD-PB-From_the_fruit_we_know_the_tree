e9c5a40f6b8d78013cac4338111012892d9a873c
Frédéric Bodin <frederic.bodin@altran.com>
2017-01-18 11:38:50 +0100

    Skeleton + Unit tests

---
+++ Project/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Node
+    {
+        public Node Left, Right;
+        public int Value;
+        public Node(int val)
+        {
+            this.Value = val;
+            this.Left = null;
+            this.Right = null;
+        }
+
+        public Node Find(int value)
+        {
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ UnitTests/Tests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+
+namespace Project
+{
+    [TestClass]
+    public class Tests
+    {
+        private Node TreeRoot;
+        
+        [TestInitialize()]
+        public void Init()
+        {
+            TreeRoot = new Node(9);
+            TreeRoot.Left = new Node(7);
+            TreeRoot.Left.Left = new Node(5);
+            TreeRoot.Left.Right = new Node(8);
+            TreeRoot.Left.Left.Left = new Node(2);
+            TreeRoot.Left.Left.Right = new Node(6);
+            TreeRoot.Right = new Node(13);
+            TreeRoot.Right.Right = new Node(17);
+            TreeRoot.Right.Left = new Node(16);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            Node n = TreeRoot.Find(8);
+            Check.That(n).IsNotNull();
+            Check.That(n.Value).Equals(8);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Node n = TreeRoot.Find(4);
+            Check.That(n).IsNull();
+        }
+    }
+}
